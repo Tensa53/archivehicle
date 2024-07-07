563c8775915b57363bb005d6214c562501f70cf9
Daniele Fabiano <danieletensa@tutamail.com>
2024-07-07 12:18:04 +0200

    Retrieving vehicles from db and displaying them on homepage

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+# Connection to mongodb
+client = MongoClient('localhost', 27017)
+
+# This is a mongodb database
+db = client.archiVehicle
+
+# This is a collection
+vehicles_col = db.vehicle
 
 @app.route('/', methods=['GET', 'POST'])
-def index():  # put application's code here
-    return render_template('index.html')
+def index():
+    vehicles_twenty = vehicles_col.find(limit=20)
+    return render_template('index.html', vehicles=vehicles_twenty)
 
 
 if __name__ == '__main__':
