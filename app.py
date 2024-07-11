35e75e29e44779eaaf2c0af1eaa009b5f10a9699
Daniele Fabiano <danieletensa@tutamail.com>
2024-07-12 01:30:31 +0200

    Initial implementation of insert vehicle feature

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ db = client.archiVehicle
 
 # This is a collection
 vehicles_col = db.vehicle
-all_vehicles = list(vehicles_col.find())
 manufacturers_col = db.manufacturer
 
 
@@ -25,12 +24,111 @@ def index():
     per_page = 24
     start = (page - 1) * per_page
     end = start + per_page
+    all_vehicles = list(vehicles_col.find())
     total_pages = math.ceil(len(all_vehicles) / per_page)
     vehicles = all_vehicles[start:end]
     return render_template('index.html', vehicles=vehicles, total_pages=total_pages, page=page)
 
 
-@app.route("/show_single_vehicle", methods=['POST'])
+def get_image_path_chassis(chassis):
+    match chassis:
+        case "Convertible":
+            return "../static/img/vehicle_sample_convertible.png"
+        case "Hatchback":
+            return "../static/img/vehicle_sample_hatchback.png"
+        case "Pickup Truck":
+            return "../static/img/vehicle_sample_pickup-truck.png"
+        case "Sedan":
+            return "../static/img/vehicle_sample_sedan.png"
+        case "Suv":
+            return "../static/img/vehicle_sample_suv.png"
+        case "Van":
+            return "../static/img/vehicle_sample_van.png"
+
+
+def get_mechanical_details_filled(form):
+    engine = form['engineInput']
+    cylinders = form['cylindersInput']
+    fuel = form['fuelInput']
+    mileage = form['mileageInput']
+    transmission = form['transmissionInput']
+
+    check = len(engine) != 0 or len(cylinders) != 0 or len(fuel) != 0 or len(mileage) != 0
+
+    if check:
+        mechanical_details = {"engine": engine, "cylinders": cylinders, "fuel": fuel, "mileage": mileage,
+                              "transmission": transmission,
+                              "drivetrain": form['drivetrainInput']}
+        return mechanical_details
+    else:
+        return None
+
+
+def get_body_details(form):
+    trim = form['trimInput']
+    doors = form['doorsInput']
+    exterior = form['excolorInput']
+    interior = form['incolorInput']
+
+    check = len(trim) != 0 or len(exterior) != 0 or len(doors) != 0 or len(interior) != 0
+
+    if check:
+        body_details = {"trim": trim, "doors": doors, "exterior": exterior, "interior": interior}
+        return body_details
+    else:
+        return None
+
+
+@app.route('/insert_vehicle', methods=['GET', 'POST'])
+def insert_vehicle():
+    if request.method == 'POST':
+        name = request.form['nameInput']
+        model = request.form['modelInput']
+        chassis = request.form['chassisInput']
+        image = get_image_path_chassis(chassis)
+        type = request.form['typeInput']
+        year = request.form['yearInput']
+        price = request.form['priceInput']
+        manufacturer = request.form['manufacturerInput']
+        description = request.form['descriptionInput']
+
+        new_vehicle = {}
+        mechanical_details = get_mechanical_details_filled(request.form)
+        body_details = get_body_details(request.form)
+
+        if body_details is None and mechanical_details is None:
+            new_vehicle = {"name": name, "model": model, "chassis": chassis, "type": type, "year": year, "price": price,
+                           "image": image,
+                           "description": description,
+                           "manufacturer_id": ObjectId(manufacturer)}
+        elif body_details is not None and mechanical_details is None:
+            new_vehicle = {"name": name, "model": model, "chassis": chassis, "type": type, "year": year, "price": price,
+                           "image": image,
+                           "description": description,
+                           "manufacturer_id": ObjectId(manufacturer),
+                           "body_details": body_details}
+        elif body_details is None and mechanical_details is not None:
+            new_vehicle = {"name": name, "model": model, "chassis": chassis, "type": type, "year": year, "price": price,
+                           "image": image,
+                           "description": description,
+                           "manufacturer_id": ObjectId(manufacturer),
+                           "mechanical_details": mechanical_details}
+        else:
+            new_vehicle = {"name": name, "model": model, "chassis": chassis, "type": type, "year": year, "price": price,
+                           "image": image,
+                           "description": description,
+                           "manufacturer_id": ObjectId(manufacturer),
+                           "mechanical_details": mechanical_details,
+                           "body_details": body_details}
+
+        vehicles_col.insert_one(new_vehicle)
+        return render_template('insert_vehicle.html', message_success="Vehicle inserted successfully")
+
+    return render_template('insert_vehicle.html', manufacturers=list(manufacturers_col.find()),
+                           drivetrains = vehicles_col.distinct("mechanical_details.drivetrain", {}))
+
+
+@app.route("/show_single_vehicle", methods=['GET', 'POST'])
 def show_single_vehicle():
     vehicle_id = request.form['vehicle_id']
     vehicle = vehicles_col.find_one({'_id': ObjectId(vehicle_id)})
